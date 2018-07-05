e300936b014bef5500ccaea61c48ca790fc86816
piyush agrawal <piyushagrawal521@gmail.com>
2018-07-05 11:02:04 +0530

    First commit

---
+++ config/database.js
@@ -0,0 +1,36 @@
+// config/database.js
+
+// configs to connect to the database
+
+//=================================USE THIS FOR ADMIN COMPUTER===================================
+// module.exports = {
+//     'connection': {
+//       // local configs.
+//         'host' : '192.168.0.9',
+//         'user': 'root',
+//         'password': 'root@SQL1',
+// 			'database': 'eqsAuction'
+//     }   
+// };
+
+//=================================USE THIS FOR OTHER COMPUTER===================================
+module.exports = {
+    'connection': {
+      // local configs.
+        'host' : '192.168.0.15',
+        'user': 'piyush',
+        'password': 'password',
+			'database': 'eqsAuction'
+    }   
+};
+
+//=================================USE THIS FOR SERVER===================================
+// module.exports = {
+//     'connection': {
+//       // server configs.
+//         'host' : 'mydbinstance.cltmsxnlpv2o.ap-south-1.rds.amazonaws.com',
+//         'user': 'aakash',
+//         'password': '123aakash',
+//         'database': 'eqsAuction'
+//     }
+// };
